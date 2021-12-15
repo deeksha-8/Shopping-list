c4f94ff42a05ebdfdb452e3be5fe456ff6a30982
deeksha-8 <91401598+deeksha-8@users.noreply.github.com>
2021-12-16 04:02:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,152 @@
-let addBtn = document.getElementById('add_btn')
-addBtn.addEventListener('click', addItem)
-let parentList = document.getElementById('parentList');
-function addItem(e) {
-    if(parentList.children(0).className == "emptyMsg"){
-        parentList.children(0).remove()
+showtask();
+let addtaskinput = document.getElementById("addtaskinput");
+let addtaskbtn = document.getElementById("addtaskbtn");
+
+addtaskbtn.addEventListener("click", function(){
+    addtaskinputval = addtaskinput.value;
+    if(addtaskinputval.trim()!=0){
+        let webtask = localStorage.getItem("localtask");
+        if(webtask == null){
+            taskObj = [];
+        }
+        else{
+            taskObj = JSON.parse(webtask);
+        }
+        taskObj.push({'task_name':addtaskinputval, 'completeStatus':false});
+
+        localStorage.setItem("localtask", JSON.stringify(taskObj));
+        addtaskinput.value = '';
     }
-    let currentBtn = e.currentTarget;
-    let currentInput = currentBtn.previousElementSibling
-    console.log(currentInput.value)
-    let currentItemName = currentInput.value
-    let currntitem;
+    showtask();
+})
 
-    let newLi = document.createElement("li")
-    // newLi.classList.add('list-group-item')
-    newLi.className = "list-group-item d-flex justify-content-between"
-    newLi.innerHTML = '<h3 class="flex-grow-1">${currentItemName}</h3> < button class="btn btn-warning mx-3" > Edit</button > <button class="btn btn-danger"  onclick="removeItem(this)">Remove</button>'
-    // newLi.textContent = currentInput.value
-   
-    parentList.appendChild(newLi)
-}
-
-let remove_btn = document.getElementById('remove_btn')
-
-remove_btn.addEventListener('click', removeItem)
-function removeItem(currElement){
-    currElement.parentElement.remove()
-    let parentList = document.getElementById('parentList');
-    if(parentList.children.length <=0){
-        let newEmptyMsg = document.createElement("h3")
-        newEmptyMsg.textContent = "Nothing is here.Add something"
-        parentList.appendChild(newEmptyMsg)
-    }
-}
-function editItem(currElement){
-    if(currElement.textContent == "done"){
-        currElement.textContent = "edit"
-        let currItemName = currElement.previousElementSibling.value
-        let currHeading = document.createElement('h3');
-        currHeading.className = "flex=grow-1"
-        currHeading.textContent = currItemName 
-        currElement.parentElement.replaceChild(currHeading , currElement.previousElementSibling)
+// showtask
+function showtask(){
+    let webtask = localStorage.getItem("localtask");
+    if(webtask == null){
+        taskObj = [];
     }
     else{
-    currElement.textContent = "done"
-    let currItemName = currElement.previousElementSibling.textContent
-    let currInput = document.createElement('input');
-    currInput.type = "text"
-    currInput.className= "form-control"
-    currInput.placeholder = "Item name"
-    currInput.value = currentItemName ;
-
-    currElement.parentElement.replaceChild(currInput , currElement.previousElementSibling)
+        taskObj = JSON.parse(webtask);
     }
+    let html = '';
+    let addedtasklist = document.getElementById("addedtasklist");
+    taskObj.forEach((item, index) => {
+
+        if(item.completeStatus==true){
+            taskCompleteValue = `<td class="completed">${item.task_name}</td>`;
+        }else{
+            taskCompleteValue = `<td>${item.task_name}</td>`;
+        }
+        html += `<tr>
+                    <th scope="row">${index+1}</th>
+                    ${taskCompleteValue}
+                    <td><button type="button" onclick="edittask(${index})" class="text-warning">Edit</button></td>
+                    <td><button type="button" class="text-success" id=${index}></i>Complete</button></td>
+                    <td><button type="button" onclick="deleteitem(${index})" class="text-danger">Delete</button></td>
+                </tr>`;
+    });
+    addedtasklist.innerHTML = html;
 }
+
+// edittask
+function edittask(index){
+    let saveindex = document.getElementById("saveindex");
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let savetaskbtn = document.getElementById("savetaskbtn");
+    saveindex.value = index;
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask); 
+    
+    addtaskinput.value = taskObj[index]['task_name'];
+    addtaskbtn.style.display="none";
+    savetaskbtn.style.display="block";
+}
+
+
+// deleteitem
+function deleteitem(index){
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask);
+    taskObj.splice(index, 1);
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    showtask();
+}
+
+
+// complete task
+let addedtasklist = document.getElementById("addedtasklist");
+    addedtasklist.addEventListener("click", function(e){
+        
+        // showtask();
+        let webtask = localStorage.getItem("localtask");
+        let taskObj = JSON.parse(webtask);
+        
+        let mytarget = e.target;
+        if(mytarget.classList[0] === 'text-success'){
+        let mytargetid = mytarget.getAttribute("id");
+        
+        
+        
+        mytargetpresibling = mytarget.parentElement.previousElementSibling.previousElementSibling;
+            
+            
+            for (keys in taskObj[mytargetid]) {
+                if(keys == 'completeStatus' && taskObj[mytargetid][keys]==true){
+                    taskObj[mytargetid].completeStatus = false;
+                 
+                }else if(keys == 'completeStatus' && taskObj[mytargetid][keys]==false){
+                    taskObj[mytargetid].completeStatus = true;
+                   
+                }
+              }
+              
+        localStorage.setItem("localtask", JSON.stringify(taskObj));
+        showtask();
+    }
+    })
+
+    
+
+
+
+// deleteall
+
+let deleteallbtn = document.getElementById("deleteallbtn");
+deleteallbtn.addEventListener("click", function(){
+    let savetaskbtn = document.getElementById("savetaskbtn");
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask);
+    if(webtask == null){
+        taskObj = [];
+    }
+    else{
+        taskObj = JSON.parse(webtask);
+        taskObj = [];
+    }
+    savetaskbtn.style.display="none";
+    addtaskbtn.style.display="block";
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    showtask();
+
+})
+
+
+// serachlist
+
+let searchtextbox = document.getElementById("searchtextbox");
+searchtextbox.addEventListener("input", function(){
+    let trlist = document.querySelectorAll("tr");
+    Array.from(trlist).forEach(function(item){
+        let searchedtext = item.getElementsByTagName("td")[0].innerText;
+        let searchtextboxval = searchtextbox.value;
+        let re = new RegExp(searchtextboxval, 'gi');
+        if(searchedtext.match(re)){
+            item.style.display="table-row";
+        }
+        else{
+            item.style.display="none";
+        }
+    })
+})
